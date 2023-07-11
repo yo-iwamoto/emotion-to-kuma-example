817c77c9a0425c22b9cbd78972bb9fc79c094719
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-07-11 23:54:29 +0900

    partially migrate to kuma

--- src/app/_components/Normal.test.tsx
+++ src/app/_components/Normal.test.tsx
@@ -1,8 +1,8 @@
-import { setupEmotionTest } from '../../__tests__/setup-function';
+import { setupKumaTest } from '../../__tests__/setup-function';
 import { Normal } from './Normal';
 import { render } from '@testing-library/react';
 
-beforeAll(() => setupEmotionTest());
+beforeAll(() => setupKumaTest());
 
 describe('Normal', () => {
   it('renders unchanged', () => {
